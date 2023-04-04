fc7975d73099992949e81fa43d26ee302b35a3f0
QUANTUM <gnunekpeku@archsolutions.online>
2023-04-04 16:14:13 +0000

    INITIAL COMMIT

---
+++ src/keycloak.js
@@ -0,0 +1,16 @@
+import Keycloak from 'keycloak-js';
+
+const keycloak = Keycloak({
+  url: 'http://your-keycloak-server/auth',
+  realm: 'your-realm',
+  clientId: 'your-client-id',
+});
+
+keycloak.init({ onLoad: 'login-required' }).then((auth) => {
+    if (!auth) {
+      window.location.reload();
+    } else {
+      console.log('Authenticated');
+    }
+  });
+  
